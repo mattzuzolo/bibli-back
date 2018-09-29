40f095b5c3365bf184e8d1f3f86e73e48ae2b411
mattzuzolo <mattzuzolo@outlook.com>
2018-09-28 20:48:21 -0400

    added route and controller methods. User can now add books to collections

--- app/controllers/api/v1/book_collections_controller.rb
+++ app/controllers/api/v1/book_collections_controller.rb
@@ -1,5 +1,17 @@
 class Api::V1::BookCollectionsController < ApplicationController
-  def employee_project_params
+
+  def index
+    @book_collections = BookCollection.all
+    render json: @book_collections.to_json()
+  end
+
+  def create
+    @book_collection = BookCollection.create(book_collection_params)
+    @book_collection.save if @book_collection.valid?
+    render json: @book_collection.to_json()
+  end
+
+  def book_collection_params
     params.require(:book_collection).permit(:book_id, :collection_id)
   end
 end
